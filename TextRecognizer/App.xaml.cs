2ba39a48ffd08790e70468bb0d36365a4a859a64
Бекзат Жолдошбеков Феликсович <bjoldoshbekov@it.gov.kg>
2024-06-04 10:28:43 +0600

    Add project files.

---
+++ TextRecognizer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TextRecognizer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TextRecognizer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TextRecognizer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
